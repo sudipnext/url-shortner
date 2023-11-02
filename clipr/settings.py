1cc77cddc91b9065bd6a410e094e8623c8fb1fb5
sudipnext <parajulisudip123@outlook.com>
2023-11-02 08:21:23 +0545

    made some changes

--- clipr/settings.py
+++ clipr/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,8 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- clipr/urls.py
+++ clipr/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -24,3 +27,5 @@ urlpatterns = [
     path('api/', include('account.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
