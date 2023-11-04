4e136106d7a43baad2e9f5524a8491a9c6d2dee6
sudipnext <parajulisudip123@outlook.com>
2023-11-04 09:15:42 +0545

    updated!

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+
 
 # Create your views here.
 @api_view(['GET'])
 @permission_classes([AllowAny,])
 def user_activation(request, uid, token):
-    return Response({'uid': uid, 'token': token})
+    target_url = f"http://localhost:3000/activate/{uid}/{token}"
+    return redirect(target_url)
